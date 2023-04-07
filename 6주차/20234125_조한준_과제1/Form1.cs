b177d7692f4ae006924d18edf1be232f06675f5b
chohanjun123 <olpeig@naver.com>
2023-04-07 16:24:09 +0900

    6주차

---
+++ 6주차/20234125_조한준_과제1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20234125_조한준_과제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ulong udata01 = ulong.Parse(textBox1.Text);
+                ulong udata02 = ulong.Parse(textBox2.Text);
+                ulong sum=0;
+                ulong mul = 1;
+
+                for (ulong i = udata01; i <= udata02; i++)
+                {
+                    mul *= i;
+                    sum += i;
+                }
+
+                label3.Text = udata01 + "부터" + udata02 + "까지의 합은" + sum + ", 곱은" + mul + "입니다";
+
+               
+
+                if (udata01 >= udata02)
+                {
+                    label3.Text = "시작값은 마지막값보다 작은 값이어야합니다!";
+                }
+                else if (udata02 > 20)
+                {
+                    label3.Text = "마지막값은 21보다 작은 값이어야합니다!";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                if (textBox1.Text == "")
+                {
+                    label3.Text = "시작값을 입력하세요";
+                }
+                else if (textBox2.Text == "")
+                {
+                    label3.Text = "마지막값을 입력하세요";
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ 6주차/20234125_조한준_과제2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20234125_조한준_과제2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                
+                    int idata01 = int.Parse(textBox1.Text);
+                    label2.Text ="";
+                    label3.Text = "";
+
+
+                    label2.Text = "수행된 i 값: ";
+                    for (int i = 1; i <= idata01; i++)
+                    {
+                        label3.Text += i + ", ";
+                        if(i % 7 == 0)
+                        {
+                            label3.Text += "\n";
+                        }
+                    }
+                    label3.Text = label3.Text.TrimEnd(',',' ');
+
+                    if (idata01 >= 21)
+                    {
+                        label3.Text = "1과 20사이의 수를 입력하세요!";
+                        label2.Text = "";
+                    }
+                    else if (idata01 <= 0)
+                    {
+                          label3.Text = "1과 20사이의 수를 입력하세요!";
+                          label2.Text = "";
+                    }
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+                label3.Text = "";
+            }
+        }
+    }
+}
