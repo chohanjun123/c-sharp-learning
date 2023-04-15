08aaf086c827e2a6e1f040a2c15ea226e4b77945
chohanjun123 <olpeig@naver.com>
2023-04-15 18:26:56 +0900

    7주차

---
+++ 7주차/20234125_조한준_과제1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20234125_조한준_과제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "";
+
+            if (a >= 2 && a <= 9 && b >= 2 && b <= 9 && a <= b)
+            {
+                label3.Text = "구구단 " + a + " 부터 " + b + " 단 까지 입니다!!";
+
+                for (int i = a; i <= b; i++)
+                {
+
+                    label3.Text += "\n\n";
+
+                    for (int j = 2; j <= 9; j++)
+                    {
+                        label3.Text += i + " * " + j + " = " + i * j + "\n";
+                    }
+
+
+                }
+
+
+            }
+
+            else
+            {
+                if (a > b)
+                {
+                    label3.Text = "시작값은 마지막값보다 크면 안돼여";
+                }
+                else
+                {
+                    label3.Text = "2 부터 9 사이의 값을 넣어주세여";
+                }
+
+            }
+
+            }
+                catch (Exception ex)
+                {
+
+                label3.Text = ex.Message;
+                
+                }
+        }
+            
+    }
+}
---
+++ 7주차/20234125_조한준_과제2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20234125_조한준_과제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int cnt = 0;
+
+                   if (a >= 1)
+                   {
+                       for (int i = 1; i <= a; i++)
+                       {
+                          if (a % i == 0)
+                          {
+                              cnt++;
+                          }
+                          if (cnt == 2)
+                          {
+                              label2.Text = a + " 는 소수입니다";
+                          }
+                          else
+                          {
+                              label2.Text = a + " 는 소수가 아닙니다";
+                          }
+
+                       }
+                   }
+                   else
+                   {
+                       label2.Text = "다른 수를 입력해주세요";
+                   }
+            }
+            catch (Exception ex)
+            {
+
+
+                label2.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
