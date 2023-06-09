ee59725253cff4d510feb24692d63cdbce9b327c
chohanjun123 <olpeig@naver.com>
2023-06-09 20:47:30 +0900

    13주차

---
+++ 13주차/20234125조한준_과제1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace _20234125조한준_과제1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.파일fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.새로만들기nToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.서식JToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.잘라내기TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.복사CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.붙여넣기VToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.삭제DToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.모두선택AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.글꼴FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.색깔CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일fToolStripMenuItem,
+            this.편집EToolStripMenuItem,
+            this.서식JToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(502, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 543);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "텍스트 파일(*.txt)|*.txt|모든 파일(*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "텍스트 파일(*.txt)|*.txt|모든 파일(*.*)|*.*";
+            // 
+            // 파일fToolStripMenuItem
+            // 
+            this.파일fToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새로만들기nToolStripMenuItem,
+            this.열기ToolStripMenuItem,
+            this.저장ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.끝내기ToolStripMenuItem});
+            this.파일fToolStripMenuItem.Name = "파일fToolStripMenuItem";
+            this.파일fToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일fToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // 새로만들기nToolStripMenuItem
+            // 
+            this.새로만들기nToolStripMenuItem.Name = "새로만들기nToolStripMenuItem";
+            this.새로만들기nToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.새로만들기nToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.새로만들기nToolStripMenuItem.Text = "새로만들기(N)";
+            this.새로만들기nToolStripMenuItem.Click += new System.EventHandler(this.새로만들기nToolStripMenuItem_Click);
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.열기ToolStripMenuItem.Text = "열기(O)";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.저장ToolStripMenuItem.Text = "저장(S)";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(189, 6);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기(X)";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // 편집EToolStripMenuItem
+            // 
+            this.편집EToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.잘라내기TToolStripMenuItem,
+            this.복사CToolStripMenuItem,
+            this.붙여넣기VToolStripMenuItem,
+            this.삭제DToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.모두선택AToolStripMenuItem});
+            this.편집EToolStripMenuItem.Name = "편집EToolStripMenuItem";
+            this.편집EToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.편집EToolStripMenuItem.Text = "편집(&E)";
+            // 
+            // 서식JToolStripMenuItem
+            // 
+            this.서식JToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.글꼴FToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.색깔CToolStripMenuItem});
+            this.서식JToolStripMenuItem.Name = "서식JToolStripMenuItem";
+            this.서식JToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.서식JToolStripMenuItem.Text = "서식(&J)";
+            // 
+            // 잘라내기TToolStripMenuItem
+            // 
+            this.잘라내기TToolStripMenuItem.Name = "잘라내기TToolStripMenuItem";
+            this.잘라내기TToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.잘라내기TToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.잘라내기TToolStripMenuItem.Text = "잘라내기(T)";
+            this.잘라내기TToolStripMenuItem.Click += new System.EventHandler(this.잘라내기TToolStripMenuItem_Click);
+            // 
+            // 복사CToolStripMenuItem
+            // 
+            this.복사CToolStripMenuItem.Name = "복사CToolStripMenuItem";
+            this.복사CToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.복사CToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.복사CToolStripMenuItem.Text = "복사(C)";
+            this.복사CToolStripMenuItem.Click += new System.EventHandler(this.복사CToolStripMenuItem_Click);
+            // 
+            // 붙여넣기VToolStripMenuItem
+            // 
+            this.붙여넣기VToolStripMenuItem.Name = "붙여넣기VToolStripMenuItem";
+            this.붙여넣기VToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.붙여넣기VToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.붙여넣기VToolStripMenuItem.Text = "붙여넣기(P)";
+            this.붙여넣기VToolStripMenuItem.Click += new System.EventHandler(this.붙여넣기VToolStripMenuItem_Click);
+            // 
+            // 삭제DToolStripMenuItem
+            // 
+            this.삭제DToolStripMenuItem.Name = "삭제DToolStripMenuItem";
+            this.삭제DToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.삭제DToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.삭제DToolStripMenuItem.Text = "삭제(D)";
+            this.삭제DToolStripMenuItem.Click += new System.EventHandler(this.삭제DToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(175, 6);
+            // 
+            // 모두선택AToolStripMenuItem
+            // 
+            this.모두선택AToolStripMenuItem.Name = "모두선택AToolStripMenuItem";
+            this.모두선택AToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.모두선택AToolStripMenuItem.Text = "모두선택(A)";
+            this.모두선택AToolStripMenuItem.Click += new System.EventHandler(this.모두선택AToolStripMenuItem_Click);
+            // 
+            // 글꼴FToolStripMenuItem
+            // 
+            this.글꼴FToolStripMenuItem.Name = "글꼴FToolStripMenuItem";
+            this.글꼴FToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.글꼴FToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.글꼴FToolStripMenuItem.Text = "글꼴(F)";
+            this.글꼴FToolStripMenuItem.Click += new System.EventHandler(this.글꼴FToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(186, 6);
+            // 
+            // 색깔CToolStripMenuItem
+            // 
+            this.색깔CToolStripMenuItem.Name = "색깔CToolStripMenuItem";
+            this.색깔CToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.색깔CToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.색깔CToolStripMenuItem.Text = "색깔(C)";
+            this.색깔CToolStripMenuItem.Click += new System.EventHandler(this.색깔CToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 569);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일fToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 새로만들기nToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 편집EToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 잘라내기TToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 복사CToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 붙여넣기VToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 삭제DToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 모두선택AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 서식JToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 글꼴FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem 색깔CToolStripMenuItem;
+    }
+}
+
---
+++ 13주차/20234125조한준_과제1/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20234125조한준_과제1
+{
+    public partial class Form1 : Form
+    {
+        bool ismodified = false;
+        bool hasfilename = false;
+        string sfilename = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ismodified = true;
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            if(ismodified == true)
+            {
+                DialogResult answer = MessageBox.Show(
+                    "변경된 내용을 저장하시겠습니다까?", "저장",
+                    MessageBoxButtons.YesNo);
+                if(answer == DialogResult.Yes)
+                {
+                    if(hasfilename == true)
+                    {
+                        System.IO.StreamWriter fs =
+                            System.IO.File.CreateText(sfilename);
+                        fs.WriteLine(textBox1.Text);
+                        fs.Close();
+                        ismodified = false;
+                    }
+                    else
+                    {
+                        if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            sfilename = saveFileDialog1.FileName;
+                            System.IO.StreamWriter fs =
+                                System.IO.File.CreateText(sfilename);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            hasfilename = true;
+                            ismodified = false;
+                        }
+                    }
+                }
+                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    sfilename = openFileDialog1.FileName;
+                    System.IO.StreamReader fs =
+                        System.IO.File.OpenText(sfilename);
+                    textBox1.Text = fs.ReadToEnd();
+                    fs.Close();
+                    hasfilename = true;
+                    ismodified = false;
+                }
+            }
+            }
+            catch
+            {
+                MessageBox.Show("열기를 하는 도중 이상이 발생했습니다.",
+                    "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 새로만들기nToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(ismodified == true)
+                {
+                    DialogResult answer = MessageBox.Show(
+                        "변경딘 내용을 저장하시겠습니까?", "저장",
+                        MessageBoxButtons.YesNo);
+                    if(answer == DialogResult.Yes)
+                    {
+                        if(hasfilename == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                System.IO.File.CreateText(sfilename);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            ismodified = false;
+                        }
+                        else
+                        {
+                            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sfilename = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                    System.IO.File.CreateText(sfilename);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasfilename = true;
+                                ismodified = false;
+                            }
+                        }
+                    }
+                }
+                textBox1.Text = "";
+                ismodified = false;
+                hasfilename = false;
+                sfilename = "";
+            }
+            catch
+            {
+                MessageBox.Show("새 파일을 준비하는 도중 이상이 발생했습니다.",
+                    "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(hasfilename == true)
+                {
+                    System.IO.StreamWriter fs =
+                        System.IO.File.CreateText(sfilename);
+                    fs.WriteLine(textBox1.Text);
+                    fs.Close();
+                    ismodified = false;
+                }
+                else
+                {
+                    if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        sfilename = saveFileDialog1.FileName;
+                        System.IO.StreamWriter fs =
+                            System.IO.File.CreateText(sfilename);
+                        fs.WriteLine(textBox1.Text);
+                        fs.Close();
+                        hasfilename = true;
+                        ismodified = false;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("저장을 하는 도중 이상이 발생했습니다.",
+    "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ismodified == true)
+                {
+                    DialogResult answer = MessageBox.Show(
+                        "변경된 내용을 저장하시겠습니까?", "저장",
+                        MessageBoxButtons.YesNo);
+                    if(answer == DialogResult.Yes)
+                    {
+                        if(hasfilename == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                System.IO.File.CreateText(sfilename);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            ismodified = false;
+                        }
+                        else
+                        {
+                            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sfilename = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                    System.IO.File.CreateText(sfilename);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasfilename = true;
+                                ismodified = false;
+                            }
+                        }
+                    }
+                }
+                Application.Exit();
+            }
+            catch
+            {
+                MessageBox.Show("끝내는 도중 이상이 발생했습니다.",
+    "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 잘라내기TToolStripMenuItem_Click(object sender, EventArgs e)
+        {             
+                textBox1.Cut();
+        }
+
+        private void 복사CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        private void 붙여넣기VToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+        private void 삭제DToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void 모두선택AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+        }
+
+        private void 글꼴FToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            textBox1.Font = fontDialog1.Font;
+
+        }
+
+        private void 색깔CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            textBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (ismodified == true)
+                {
+                    DialogResult answer = MessageBox.Show(
+                        "변경된 내용을 저장하시겠습니까?", "저장",
+                        MessageBoxButtons.YesNo);
+                    if (answer == DialogResult.Yes)
+                    {
+                        if (hasfilename == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                System.IO.File.CreateText(sfilename);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            ismodified = false;
+                        }
+                        else
+                        {
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sfilename = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                    System.IO.File.CreateText(sfilename);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasfilename = true;
+                                ismodified = false;
+                            }
+                        }
+                    }
+                }
+                Application.Exit();
+            }
+            catch
+            {
+                MessageBox.Show("끝내는 도중 이상이 발생했습니다.",
+    "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
